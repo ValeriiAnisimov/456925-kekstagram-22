d4b907e10feaa548ff9a0069141c11f97da43036
Valerii Anisimov <valerii@valeriis-MacBook-Pro.local>
2021-01-30 13:53:37 +0300

    Выполнил задание "Нужно больше функций"

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function getRandomIntInclusive(min, max) {
+  if(min >= max || min < 0) return 'Incorrect incoming data !';
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+isStrLengthValid(str,maxStrLength) {
+  if str.length <= maxStrLength return true
+  return false
+}
+
