0200feb1b8c96fd8da1ef2b7e577749990d30570
valerii <valerii@valeriis-iMac.local>
2021-02-20 17:05:39 +0500

    Разбил код на модули

---
+++ js/util.js
@@ -0,0 +1,14 @@
+const getRandomIntInclusive = function (min, max) {
+  if(min >= max || min < 0) return 'Incorrect incoming data !';
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const isStrLengthValid = function (str, maxStrLength) {
+  if (str.length <= maxStrLength) return true
+  return false
+}
+
+export {getRandomIntInclusive, isStrLengthValid};
